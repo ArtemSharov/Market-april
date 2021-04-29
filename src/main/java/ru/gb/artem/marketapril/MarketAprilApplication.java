c1977fa340c5768c244365881133e104585e5fb5
Artem Sharov <sharov.a.p.10@gmail.com>
2021-04-29 22:01:31 +0300

    first commit

---
+++ src/main/java/ru/gb/artem/marketapril/MarketAprilApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.artem.marketapril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAprilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketAprilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/artem/marketapril/MarketAprilApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.artem.marketapril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAprilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
