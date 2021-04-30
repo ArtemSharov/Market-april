1e6087ef4e1bb4938e3da4e6f6691865e74f1340
Artem Sharov <sharov.a.p.10@gmail.com>
2021-04-30 13:35:03 +0300

    complete

---
+++ src/main/java/ru/gb/artem/marketapril/models/Product.java
@@ -0,0 +1,25 @@
+package ru.gb.artem.marketapril.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private int price;
+}
